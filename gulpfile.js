175955d01ef79e0640a23f67103bf776ce9d1d69
nanonanomachine <koyama.gakugai@gmail.com>
2016-02-20 16:12:07 +0900

    Add compiling haml and outputting js and html files to app directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
-gulp.task('compile', function(){
+gulp.task('compile', ['compile-babel', 'compile-haml']);
+
+gulp.task('compile-babel', function(){
   return gulp.src('src/**/*.js')
     .pipe($.babel({
         presets:['es2015']
      }))
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('app'));
  });
+
+gulp.task('compile-haml', function(){
+  return gulp.src("static/**/*.haml")
+    .pipe($.haml())
+    .pipe(gulp.dest('app'));
+})
